2c753a34c07d3c70f732655e7e91ee58cf85f870
Javier Lete <javier@eskura21.com>
2022-10-26 17:36:35 +0200

    Persona con static

--- Bases/src/com/ipartek/formacion/uf1305/poo/Persona.java
+++ Bases/src/com/ipartek/formacion/uf1305/poo/Persona.java
@@ -5,13 +5,13 @@ import java.time.Period;
 
 // Clase (como un molde)
 public class Persona {
-	private static final String NOMBRE_VACIO = "ANÓNIMO";
+	public static final String NOMBRE_VACIO = "ANÓNIMO";
 	// Variables de instancia
 	private Long id;
 	private String nombre;
 	private String apellido;
 	private LocalDate fechaNacimiento;
-	
+
 	// Constructores
 	public Persona(Long id, String nombre, String apellido, LocalDate fechaNacimiento) {
 		setId(id);
@@ -19,103 +19,104 @@ public class Persona {
 		setApellido(apellido);
 		setFechaNacimiento(fechaNacimiento);
 	}
-	
+
 	public Persona(Long id, String nombre, String apellido) {
 		this(id, nombre, apellido, null);
 	}
-	
+
 	public Persona(String nombre, String apellido) {
 		this(null, nombre, apellido, null);
 	}
-	
+
 	public Persona(String nombre) {
 		this(null, nombre, null, null);
 	}
-	
+
+	// Constructor de copia
+	public Persona(Persona persona) {
+		this(persona.getId(), persona.getNombre(), persona.getApellido(), persona.getFechaNacimiento());
+	}
+
 	public Persona() {
 		this(null, NOMBRE_VACIO, null, null);
 	}
-	
+
 	// Getters y setters
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
-		if(nombre == null) {
+		if (nombre == null) {
 			throw new RuntimeException("No se admiten nombres nulos");
 		}
-		
-		if(nombre.trim().length() == 0) {
+
+		if (nombre.trim().length() == 0) {
 			throw new RuntimeException("No se admiten nombres de longitud 0");
 		}
-		
+
 		this.nombre = nombre.trim();
 	}
+
 	public String getApellido() {
 		return apellido;
 	}
+
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
+
 	public LocalDate getFechaNacimiento() {
 		return fechaNacimiento;
 	}
-	
+
 	public void setFechaNacimiento(LocalDate fechaNacimiento) {
 		this.fechaNacimiento = fechaNacimiento;
 	}
-	
+
 	// Métodos de instancia
 	public String getNombreCompleto() {
 		return nombre + " " + apellido;
 	}
-	
+
 	public int getEdad() {
 		LocalDate hoy = LocalDate.now();
 		int anyoActual = hoy.getYear();
 		int anyoNacimiento = fechaNacimiento.getYear();
 		int anyos = anyoActual - anyoNacimiento;
-		
+
 		LocalDate cumpleanyosActual = fechaNacimiento.withYear(anyoActual);
-		
-		if(hoy.isBefore(cumpleanyosActual)) {
+
+		if (hoy.isBefore(cumpleanyosActual)) {
 			anyos--;
 		}
-		
+
 		return anyos;
 	}
-	
+
 	public int getAnyos() {
 		Period p = Period.between(fechaNacimiento, LocalDate.now());
-		
+
 		return p.getYears();
 	}
 
+	public boolean isMayorQue(Persona persona) {
+		return this.fechaNacimiento.isBefore(persona.fechaNacimiento);
+	}
+
+	public static boolean isMayorQue(Persona p1, Persona p2) {
+		return p1.fechaNacimiento.isBefore(p2.fechaNacimiento);
+	}
+
+	public static Persona getMayor(Persona p1, Persona p2) {
+		return isMayorQue(p1, p2) ? p1 : p2;
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Bases/src/com/ipartek/formacion/uf1305/poo/PersonaPrueba.java
+++ Bases/src/com/ipartek/formacion/uf1305/poo/PersonaPrueba.java
@@ -28,5 +28,66 @@ public class PersonaPrueba {
 		
 		System.out.println(p.getEdad());
 		System.out.println(p.getAnyos());
+		
+		Persona p2 = new Persona(p);
+		
+		p2.setNombre("SOY UNA COPIA");
+		
+		System.out.println(p.getNombreCompleto());
+		System.out.println(p2.getNombreCompleto());
+		
+		p.setNombre("Juan");
+		
+		if(p.getNombre() == new String("Juan")) {
+			System.out.println("Está en la misma dirección de memoria");
+		} else {
+			System.out.println("No está en la misma dirección de memoria");
+		}
+		
+		if(p.getNombre().equals(new String("Juan"))) {
+			System.out.println("Sí se llama Juan");
+		} else {
+			System.out.println("No se llama Juan");
+		}
+		
+		//p.setApellido(null);
+		
+		System.out.println(p.getApellido() instanceof String);
+		
+		if("González".equals(p.getApellido())) {
+			System.out.println("Sí se apellida González");
+		} else {
+			System.out.println("No se apellida González");
+		}
+		
+		Persona per1 = new Persona(1L, "Nombre", "Apellido", LocalDate.of(1234, 1, 2));
+		Persona per2 = new Persona(1L, "Nombre", "Apellido", LocalDate.of(1234, 1, 2));
+		
+		if(per1 == per2) {
+			System.out.println("Son el mismo");
+		}
+		
+		// TODO Pendiente de herencia
+		if(per1.equals(per2)) {
+			System.out.println("Son iguales");
+		}
+		
+		System.out.println(Persona.NOMBRE_VACIO);
+		
+		System.out.println(Math.sqrt(2));
+		
+		per2.setFechaNacimiento(LocalDate.now());
+		
+		System.out.println(per1.isMayorQue(per2));
+		
+		System.out.println(Persona.isMayorQue(per1, per2));
+		
+		System.out.println(Persona.getMayor(per1, per2).getNombreCompleto().toUpperCase());
+		
+		Persona mayor = Persona.getMayor(per1, per2);
+		String nombreCompleto = mayor.getNombreCompleto();
+		String nombreCompletoEnMayusculas = nombreCompleto.toUpperCase();
+		
+		System.out.println(nombreCompletoEnMayusculas);
 	}
 }
