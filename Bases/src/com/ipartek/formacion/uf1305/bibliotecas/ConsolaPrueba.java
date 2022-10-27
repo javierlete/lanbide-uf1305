4a233ab2e49d0fbb72eab641c15cd0541da5eb7d
Javier Lete <javier@eskura21.com>
2022-10-27 17:48:05 +0200

    Ejemplo de import static con Math y comparación de Consola con System

--- Bases/src/com/ipartek/formacion/uf1305/bibliotecas/ConsolaPrueba.java
+++ Bases/src/com/ipartek/formacion/uf1305/bibliotecas/ConsolaPrueba.java
@@ -1,10 +1,25 @@
 package com.ipartek.formacion.uf1305.bibliotecas;
 
 import static com.ipartek.formacion.uf1305.bibliotecas.Consola.*;
+import static java.lang.Math.*;
 
 public class ConsolaPrueba {
 
 	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.println("Hola a todos");
+//		
+//		System.out.print("Dime un número: ");
+//		int i = sc.nextInt();
+//		sc.nextLine();
+//		
+//		System.out.print("Dime un texto: ");
+//		String s = sc.nextLine();
+//		
+//		System.out.println(i);
+//		System.out.println("[" + s + "]");
+
 		pl("Hola a todos");
 		
 		int i = getInt("Dime un número");
@@ -13,6 +28,8 @@ public class ConsolaPrueba {
 		
 		pl(i);
 		pl("[" + s + "]");
+		
+		pl(Math.sin(5));
+		pl(sqrt(5));
 	}
-
 }
