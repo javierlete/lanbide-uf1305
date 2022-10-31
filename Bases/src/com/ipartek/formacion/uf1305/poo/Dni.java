cba7862c017da1ea7e46b1c7d004baa4c98d396a
Javier Lete <javier@eskura21.com>
2022-10-31 17:35:22 +0100

    DNI mejorado con validación real

--- Bases/src/com/ipartek/formacion/uf1305/poo/Dni.java
+++ Bases/src/com/ipartek/formacion/uf1305/poo/Dni.java
@@ -1,22 +1,72 @@
 package com.ipartek.formacion.uf1305.poo;
 
 public class Dni {
-	private String numeroYLetra;
-	
+	private static final String REGEX_DNI = "[\\dXYZ]\\d{7}[A-Z]";
+	private static final String LETRAS_DNI = "TRWAGMYFPDXBNJZSQVHLCKE";
+
+	private String numeroYLetraValido;
+
 	public Dni(String numeroYLetra) {
-		if(validarDni(numeroYLetra)) {
-			this.numeroYLetra = numeroYLetra;
+		if (validarDni(numeroYLetra)) {
+			this.numeroYLetraValido = numeroYLetra.toUpperCase();
 		} else {
-			throw new RuntimeException("El DNI debe tener 9 caracteres");
+			throw new RuntimeException("El DNI no es válido");
 		}
 	}
 
-	public static boolean validarDni(String numeroYLetra) {
-		return numeroYLetra.length() == 9; 
+	public String getNumeroYLetra() {
+		return numeroYLetraValido;
 	}
-	
+
+	public boolean isExtranjero() {
+		char primerCaracter = numeroYLetraValido.charAt(0);
+
+		boolean esExtranjero = primerCaracter == 'X' || primerCaracter == 'Y' || primerCaracter == 'Z';
+
+		return esExtranjero;
+	}
+
+	public char getLetra() {
+		char letra = numeroYLetraValido.charAt(8);
+		return letra;
+	}
+
+	public String getNumero() {
+		String numero = numeroYLetraValido.substring(0, 8);
+
+		return numero;
+	}
+
+	public static boolean validarDni(String numeroYLetraAValidar) {
+		if (numeroYLetraAValidar == null) {
+			return false;
+			// throw new RuntimeException("El número y letra no pueden ser nulos");
+		}
+
+		numeroYLetraAValidar = numeroYLetraAValidar.toUpperCase();
+
+		if (!numeroYLetraAValidar.matches(REGEX_DNI)) {
+			return false;
+			// throw new RuntimeException("El formato del DNI debe ser 12345678A o
+			// X1234567B");
+		}
+
+		String numeroSinLetraDelFinal = numeroYLetraAValidar.substring(0, 8); // 12345678 o X234567
+		String numeroSoloConDigitos = numeroSinLetraDelFinal.replace('X', '0').replace('Y', '1').replace('Z', '2'); // 12345678
+
+		int numero = Integer.parseInt(numeroSoloConDigitos);
+		char letra = numeroYLetraAValidar.charAt(8);
+		int restoDeLaDivisionEntera = numero % 23;
+
+		char letraCorrecta = LETRAS_DNI.charAt(restoDeLaDivisionEntera);
+
+		boolean dniCorrecto = letra == letraCorrecta;
+
+		return dniCorrecto;
+	}
+
 	@Override
 	public String toString() {
-		return "Dni [numeroYLetra=" + numeroYLetra + "]";
+		return "Dni [numeroYLetra=" + numeroYLetraValido + "]";
 	}
 }
--- Bases/src/com/ipartek/formacion/uf1305/poo/DniPrueba.java
+++ Bases/src/com/ipartek/formacion/uf1305/poo/DniPrueba.java
@@ -3,13 +3,20 @@ package com.ipartek.formacion.uf1305.poo;
 public class DniPrueba {
 
 	public static void main(String[] args) {
-		Dni d = new Dni("12345678A");
+		System.out.println(Dni.validarDni("12345678Z"));
+		
+		Dni d = new Dni("Y2345678Z");
+		
+		Dni d2 = new Dni("12345678z");
 		
 		Persona p = new Persona();
 		
 		p.setDni(d);
 		
 		System.out.println(p);
+		
+		System.out.println(d);
+		System.out.println(d2);
 	}
 
 }
