9f8b2f42e622bf0b65ed085b4f795feb97feba76
Javier Lete <javier@eskura21.com>
2022-11-10 19:32:26 +0100

    Versión con borrado lógico y no físico

--- ejemplojdbc/src/main/java/com/ipartek/formacion/ejemplojdbc/App.java
+++ ejemplojdbc/src/main/java/com/ipartek/formacion/ejemplojdbc/App.java
@@ -4,11 +4,14 @@ import java.sql.*;
 
 public class App 
 {
-	private static final String SQL_SELECT = "SELECT * FROM usuarios";
-	private static final String SQL_SELECT_ID = "SELECT * FROM usuarios WHERE id=?";
+	private static final String SQL_SELECT = "SELECT * FROM usuarios WHERE estado = 1";
+	private static final String SQL_SELECT_ID = "SELECT * FROM usuarios WHERE id = ? AND estado = 1";
 	private static final String SQL_INSERT = "INSERT INTO usuarios (nombre, email, password) VALUES (?,?,?)";
-	private static final String SQL_UPDATE = "UPDATE usuarios SET nombre=?,email=?,password=? WHERE id=?";
-	private static final String SQL_DELETE = "DELETE FROM usuarios WHERE id=?";
+	private static final String SQL_UPDATE = "UPDATE usuarios SET nombre=?,email=?,password=? WHERE id = ? AND estado = 1";
+	private static final String SQL_DELETE = "UPDATE usuarios SET estado = 0 WHERE id = ? AND estado = 1";
+	
+	private static final String SQL_SELECT_BORRADOS = "SELECT * FROM usuarios WHERE estado = 0";
+	private static final String SQL_RECUPERAR = "UPDATE usuarios SET estado = 1 WHERE id = ? AND estado = 0";
 	// final String URL = "jdbc:mysql://localhost:3306/basededatosoesquema";
     private static final String URL = "jdbc:sqlite:tienda.db";
     // final String USER = "root";
@@ -16,8 +19,13 @@ public class App
     
     public static void main( String[] args ) throws SQLException
     {
-    	borrar(2L);
+    	borrar(1L);
         obtenerTodos();
+        obtenerBorrados();
+        recuperar(1L);
+        obtenerTodos();
+        obtenerBorrados();
+        
     	// obtenerPorId(1L);
         // insertar("Pedro", "pedro@email.net", "contrapedro");
         // modificar(1L, "Javier", "javier@email.net", "contraseña");
@@ -28,6 +36,20 @@ public class App
 		Connection con = DriverManager.getConnection(URL); // , USER, PASS);
         Statement st = con.createStatement();
         ResultSet rs = st.executeQuery(SQL_SELECT);
+        
+        System.out.println("TODOS");
+        
+        while(rs.next()) {
+        	System.out.printf("%2s %-20s %-20s %-20s\n", rs.getLong("id"), rs.getString("nombre"), rs.getString("email"), rs.getString("password"));	
+        }
+	}
+	
+	private static void obtenerBorrados() throws SQLException {
+		Connection con = DriverManager.getConnection(URL); // , USER, PASS);
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(SQL_SELECT_BORRADOS);
+        
+        System.out.println("BORRADOS");
         
         while(rs.next()) {
         	System.out.printf("%2s %-20s %-20s %-20s\n", rs.getLong("id"), rs.getString("nombre"), rs.getString("email"), rs.getString("password"));	
@@ -79,6 +101,17 @@ public class App
 	private static void borrar(Long id) throws SQLException {
 		Connection con = DriverManager.getConnection(URL); // , USER, PASS);
         PreparedStatement pst = con.prepareStatement(SQL_DELETE);
+        
+        pst.setLong(1, id);
+        
+        int numRegMod = pst.executeUpdate();
+
+        System.out.println(numRegMod);
+	}
+	
+	private static void recuperar(Long id) throws SQLException {
+		Connection con = DriverManager.getConnection(URL); // , USER, PASS);
+        PreparedStatement pst = con.prepareStatement(SQL_RECUPERAR);
         
         pst.setLong(1, id);
         
